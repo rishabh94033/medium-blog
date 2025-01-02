c2012233dff1e02b6aaf8f62e72baf45c06c6dad
rishabhs2108 <developerrishabh21@gmail.com>
2025-01-03 03:49:37 +0530

    Added some frontend pages and wrote components

---
+++ frontend/src/components/quote.tsx
@@ -0,0 +1,19 @@
+export default function Quote(){
+    return(
+        <div className="bg-slate-200 h-screen flex-col self-end justify-center content-center font-sans">
+            <div className=" items-center flex flex-col">
+                <div className="max-w-xl">
+            <div className=" text-black font-semibold text-2xl">
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem qui exercitationem atque? Reiciendis totam doloribus ad suscipit ullam veniam non"
+            </div>
+            <div  className="text-black font-semibold text-xl mt-2">
+                Jules Winfield
+            </div>
+            <div className=" text-slate-600 font-medium text-lg">
+                CEO, Amk ltd
+            </div>
+                </div>
+            </div>
+        </div>
+    )
+}
